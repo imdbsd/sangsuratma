afb0d62bc51b59628e8673c2daf73959b996c832
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-01-12 15:49:26 +0800

    test(event-scrapper): Add test for eventScrapper (#1)
    * add jest
    
    *  config jest
    
    * improve typing
    
    * add constants for testing
    
    * add test for scrap event january 2020

---
+++ src/__tests__/constants.ts
@@ -0,0 +1,64 @@
+export const MONTH_SCRAPPING_TEST = 1
+export const YEAR_SCRAPPING_TEST = 2020
+export const EVENT_JANUARY_2020 = [
+  {
+    eventName: 'Hari Purnama Kapitu',
+    eventType: 'rerainan',
+    timestamp: '2020-01-10T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Buda Kliwon Gumbreg',
+    eventType: 'rerainan',
+    timestamp: '2020-01-15T00:00:00.000Z',
+  },
+  {
+    eventName: 'Kajeng Kliwon Enyitan',
+    eventType: 'rerainan',
+    timestamp: '2020-01-15T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Suci Siwalatri',
+    eventType: 'rerainan',
+    timestamp: '2020-01-23T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Tilem Sasih Kapitu',
+    eventType: 'rerainan',
+    timestamp: '2020-01-24T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Tumpek Uduh/Pengatag',
+    eventType: 'rerainan',
+    timestamp: '2020-01-25T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Buda Cemeng Warigadean',
+    eventType: 'rerainan',
+    timestamp: '2020-01-29T00:00:00.000Z',
+  },
+  {
+    eventName: 'Kajeng Kliwon Uwudan',
+    eventType: 'rerainan',
+    timestamp: '2020-01-30T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Bhatari Sri',
+    eventType: 'rerainan',
+    timestamp: '2020-01-31T00:00:00.000Z',
+  },
+  {
+    eventName: 'Tahun Baru',
+    eventType: 'hari peringatan',
+    timestamp: '2020-01-01T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Peringatan Tritura',
+    eventType: 'hari peringatan',
+    timestamp: '2020-01-10T00:00:00.000Z',
+  },
+  {
+    eventName: 'Hari Kusta Internasional',
+    eventType: 'hari peringatan',
+    timestamp: '2020-01-25T00:00:00.000Z',
+  },
+]
