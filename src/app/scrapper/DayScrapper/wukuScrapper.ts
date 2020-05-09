f4fb4e53d2ae6d90a4abca4dbd8f87562be47d82
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-09 21:32:01 +0800

    add wuku scrapper

---
+++ src/app/scrapper/DayScrapper/wukuScrapper.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+import { DayScrapperParams as Params, Wuku } from '../../types'
+import { isValidWuku } from '../utils'
+
+const CENTER_UP_CELL_SELECTOR = '.isitanggal.hitam.tengahbawah'
+
+const wukuScrapper = async (params: Params): Promise<Wuku | null> => {
+  try {
+    const { data: html } = await axios(
+      `http://www.kalenderbali.info/kalender/detailHari/${params.date}/${params.month}/${params.year}/hitam/html`
+    )
+    const $ = cheerio.load(html)
+    const centerUpCell = $(CENTER_UP_CELL_SELECTOR).html()
+
+    if (!centerUpCell) throw new Error('No centerUpCell content')
+
+    const centerUpCellContent = centerUpCell.trim().split('<br>')
+    if (!isValidWuku(centerUpCellContent[1])) return null
+    return centerUpCellContent[1]
+  } catch (e) {
+    return null
+  }
+}
+
+export default wukuScrapper
