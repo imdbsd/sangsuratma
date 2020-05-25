c355f928aba359ac501a3d5466f1756b162b6eea
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-25 22:08:36 +0800

    fix validation test

--- __tests__/validator-test/validateDayParams.test.ts
+++ __tests__/validator-test/validateDayParams.test.ts
@@ -2,35 +2,31 @@ import validateDayParams from '../../src/App/Shared/Validator/validateDayParams'
 
 describe('Validate Day Params Test', () => {
   test('Test invalid params string', async () => {
-    const { isValid, data, error } = await validateDayParams('invalid')
-    expect(isValid).toBeFalsy()
-    expect(data).toBeUndefined()
-    expect(error).toBeInstanceOf(Error)
+    try {
+      await validateDayParams('invalid')
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error)
+    }
   })
 
   test('Test invalid params object', async () => {
-    const { isValid, data, error } = await validateDayParams({
-      date: '-1',
-      month: '1',
-      year: '2020',
-    })
-    expect(isValid).toBeFalsy()
-    expect(data).toBeUndefined()
-    expect(error).toBeInstanceOf(Error)
+    try {
+      await validateDayParams({
+        date: '-1',
+        month: '1',
+        year: '2020',
+      })
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error)
+    }
   })
 
   test('Test validate params', async () => {
-    const { isValid, data, error } = await validateDayParams({
+    const isValid = await validateDayParams({
       date: '22',
       month: '1',
       year: '2020',
     })
     expect(isValid).toBeTruthy()
-    expect(data).toEqual({
-      date: 22,
-      month: 1,
-      year: 2020,
-    })
-    expect(error).toBeUndefined()
   })
 })
