18b585aa00485404570618f45e2acd25d9173f3a
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-25 21:47:42 +0800

    add test

---
+++ __tests__/request-test/penyalin.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../../src/app'
+import { queryString, DAY_22_JANUARY_2020 } from '../constants'
+
+describe('Test requets', () => {
+  test('Request wuku from 22 Januari 2020', async () => {
+    const res = await request(app).get(`/penyalin/wuku?${queryString}`)
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      wuku: DAY_22_JANUARY_2020.wuku,
+    })
+  })
+})
