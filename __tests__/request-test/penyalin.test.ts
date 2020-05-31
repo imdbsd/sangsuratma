5e68a1b95a291847b316d69a1f8bc072b3323b2a
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-31 20:53:36 +0800

    add timeout test

--- __tests__/request-test/penyalin.test.ts
+++ __tests__/request-test/penyalin.test.ts
@@ -3,6 +3,9 @@ import app from '../../src/App/app'
 import { queryString, DAY_22_JANUARY_2020 } from '../constants'
 
 describe('API requests test', () => {
+  beforeEach(() => {
+    jest.setTimeout(10000)
+  })
   test('Request wuku from 22 Januari 2020', async () => {
     const res = await request(app).get(`/penyalin/wuku?${queryString}`)
 
@@ -40,6 +43,38 @@ describe('API requests test', () => {
     expect(res.header['content-type']).toContain('application/json')
     expect(res.body).toStrictEqual({
       bhatara: DAY_22_JANUARY_2020.bhatara,
+    })
+  })
+
+  test('Request urip from 22 Januari 2020', async () => {
+    const res = await request(app).get(`/penyalin/urip?${queryString}`)
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      urip: DAY_22_JANUARY_2020.urip,
+    })
+  })
+
+  test('Request wewaran from 22 Januari 2020', async () => {
+    const res = await request(app).get(`/penyalin/wewaran?${queryString}`)
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      wewaran: DAY_22_JANUARY_2020.wewaran,
+    })
+  })
+
+  test('Request penanggal pangelong from 22 Januari 2020', async () => {
+    const res = await request(app).get(
+      `/penyalin/penanggal-pangelong?${queryString}`
+    )
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      penanggalPangelong: DAY_22_JANUARY_2020.penanggal_pangelong,
     })
   })
 })
