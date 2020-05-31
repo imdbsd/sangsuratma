86fdbb8a87da2e704ba203f258eeb793d4a7ab3f
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-31 15:46:27 +0800

    add test sasih

--- __tests__/request-test/penyalin.test.ts
+++ __tests__/request-test/penyalin.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../../src/App/app'
 import { queryString, DAY_22_JANUARY_2020 } from '../constants'
 
-describe('Test requets', () => {
+describe('API requests test', () => {
   test('Request wuku from 22 Januari 2020', async () => {
     const res = await request(app).get(`/penyalin/wuku?${queryString}`)
 
@@ -10,6 +10,16 @@ describe('Test requets', () => {
     expect(res.header['content-type']).toContain('application/json')
     expect(res.body).toStrictEqual({
       wuku: DAY_22_JANUARY_2020.wuku,
+    })
+  })
+
+  test('Request sasih from 22 Januari 2020', async () => {
+    const res = await request(app).get(`/penyalin/sasih?${queryString}`)
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      wuku: DAY_22_JANUARY_2020.sasih,
     })
   })
 })
