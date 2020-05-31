6e31a1659c92db48fd8e35c30002c4b06547e69c
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-31 21:27:02 +0800

    add events test

--- __tests__/request-test/penyalin.test.ts
+++ __tests__/request-test/penyalin.test.ts
@@ -1,13 +1,19 @@
 import request from 'supertest'
 import app from '../../src/App/app'
-import { queryString, DAY_22_JANUARY_2020 } from '../constants'
+import {
+  queryString,
+  DAY_22_JANUARY_2020,
+  EVENT_15_JANUARY_2020,
+} from '../constants'
 
 describe('API requests test', () => {
   beforeEach(() => {
     jest.setTimeout(10000)
   })
   test('Request wuku from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/wuku?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/wuku?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
@@ -17,7 +23,9 @@ describe('API requests test', () => {
   })
 
   test('Request sasih from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/sasih?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/sasih?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
@@ -27,7 +35,9 @@ describe('API requests test', () => {
   })
 
   test('Request ingkel from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/ingkel?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/ingkel?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
@@ -37,7 +47,9 @@ describe('API requests test', () => {
   })
 
   test('Request bhatara from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/bhatara?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/bhatara?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
@@ -47,7 +59,9 @@ describe('API requests test', () => {
   })
 
   test('Request urip from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/urip?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/urip?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
@@ -57,7 +71,9 @@ describe('API requests test', () => {
   })
 
   test('Request wewaran from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/wewaran?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/wewaran?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
@@ -68,7 +84,7 @@ describe('API requests test', () => {
 
   test('Request penanggal pangelong from 22 Januari 2020', async () => {
     const res = await request(app).get(
-      `/penyalin/penanggal-pangelong?${queryString}`
+      `/penyalin/penanggal-pangelong?${queryString['22_JANUARY_2020']}`
     )
 
     expect(res.status).toBe(200)
@@ -79,10 +95,22 @@ describe('API requests test', () => {
   })
 
   test('Request day data from 22 Januari 2020', async () => {
-    const res = await request(app).get(`/penyalin/day?${queryString}`)
+    const res = await request(app).get(
+      `/penyalin/day?${queryString['22_JANUARY_2020']}`
+    )
 
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toContain('application/json')
     expect(res.body).toStrictEqual(DAY_22_JANUARY_2020)
+  })
+
+  test('Request events from 15 Januari 2020', async () => {
+    const res = await request(app).get(
+      `/penyalin/events?${queryString['15_JANUARY_2020']}`
+    )
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual(EVENT_15_JANUARY_2020)
   })
 })
