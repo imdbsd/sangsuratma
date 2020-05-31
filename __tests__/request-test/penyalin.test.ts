66058c79f9b0f157b289911863d3abe69d1ee8dd
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-31 16:03:49 +0800

    add 2 new test

--- __tests__/request-test/penyalin.test.ts
+++ __tests__/request-test/penyalin.test.ts
@@ -22,4 +22,24 @@ describe('API requests test', () => {
       sasih: DAY_22_JANUARY_2020.sasih,
     })
   })
+
+  test('Request ingkel from 22 Januari 2020', async () => {
+    const res = await request(app).get(`/penyalin/ingkel?${queryString}`)
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      ingkel: DAY_22_JANUARY_2020.ingkel,
+    })
+  })
+
+  test('Request bhatara from 22 Januari 2020', async () => {
+    const res = await request(app).get(`/penyalin/bhatara?${queryString}`)
+
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toContain('application/json')
+    expect(res.body).toStrictEqual({
+      bhatara: DAY_22_JANUARY_2020.bhatara,
+    })
+  })
 })
